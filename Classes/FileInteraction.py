25a4b3751a89b9874cd962ae4a3ad4952e93ad2f
Briot--Amadieu Kellyan <kellyan.briot-amadieu01@etu.umontpellier.fr>
2026-02-05 17:39:59 +0100

    Implémentation FileInteraction SANS TESTS

--- Classes/FileInteraction.py
+++ Classes/FileInteraction.py
@@ -1,13 +1,23 @@
+import shutil
+import os
+from ParserError import ParserError
+
 class FileInteraction:
     def __init__(self,directory : str = "../sudoku_parser_out",file : str = "file_out.txt"):
-        self.directory=directory
+        self.setDirectory(directory)
         self.file=file
     
     def getDirectory(self) -> str :
         return self.directory
     
     def setDirectory(self,directory : str) -> None:
-        self.directory = directory
+        if os.path.isdir(directory):
+            self.directory=directory
+        else:
+            if directory==directory=="../sudoku_parser_out":
+                os.makedirs(directory)
+            else:
+                raise(ParserError("FileInteraction : Erreur lors de la création de l'objet -> directory non existant"))
     
     def getFile(self) -> str:
         return self.file
@@ -15,20 +25,47 @@ class FileInteraction:
     def setFile(self, file : str) -> None:
         self.file = file
     
+    # Ecrit de manière brute le contenue passer en paramètre /!\ PAS DE FORMATTAGE /!\
     def write(self,content : str) -> None:
-        pass
+        f=open(self.directory+self.file)
+        f.write(content)
+        f.flush()
+        f.close()
     
-    def read() -> str:
-        return ""
+    def read(self) -> list[list[str]]:
+        f=open(self.directory+self.file)
+        content=f.readlines()
+        f.close()
+        return content
     
-    def writeFile(content : str, file : str) -> None:
-        pass
+    # Ecrit de manière brute le contenue passer en paramètre /!\ PAS DE FORMATTAGE /!\
+    def writeFile(self,content : str, file : str) -> None:
+        self.setFile(file)
+        self.write(content)
     
-    def readFile(file : str) -> str:
-        return ""
+    def readFile(self,file : str) -> list[list[str]]:
+        self.setFile(file)
+        if os.path.isfile(self.directory+self.file):
+            print("FileInteraction : /!\ WARNING /!\ [READ] Fichier non existant")
+            return [[""]]
+        else:
+            return self.read()
     
-    def writeFileDirectory(content : str, file : str, directory : str) -> None:
-        pass
+    # Ecrit de manière brute le contenue passer en paramètre /!\ PAS DE FORMATTAGE /!\
+    def writeFileDirectory(self,content : str, file : str, directory : str) -> None:
+        self.setFile(file)
+        self.setDirectory(directory)
+        self.write(content)
     
-    def readFileDirectory(file : str, directory : str) -> str:
-        return ""
+    def readFileDirectory(self,file : str, directory : str) -> list[list[str]]:
+        self.setFile(file)
+        self.setDirectory(directory)
+        if os.path.isfile(self.directory+self.file):
+            print("FileInteraction : /!\ WARNING /!\ [READ] Fichier non existant")
+            return [[""]]
+        else:
+            return self.read()
+    
+    def clearDirectory(self) -> None:
+        shutil.rmtree(self.directory)
+        os.makedirs(self.directory)
