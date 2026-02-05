b30ecd3b28da9dc759b4fce397f8b718c18c71e7
martinkonchev <martin.konchev@etu.umontpellier.fr>
2026-02-05 17:42:34 +0100

    Interface classes

--- interfaces/InterfaceConsole.py
+++ interfaces/InterfaceConsole.py
@@ -13,23 +13,23 @@ class InterfaceConsole(Interface): # extends Interface
     def startPlaying(self): # demande le choix: si 1: jouer Sudoku; si 2: quitter
         pass
 
-    def demanderChoix(self) -> str: # soit generer une grille et jouer, soit quitter
+    def askChoice(self) -> str: # soit generer une grille et jouer, soit quitter
         pass
 
-    def demanderDifficulte(self) -> Difficulte: # demande la difficulté à propos de nbr de retraites
+    def askDifficulty(self) -> Difficulte: # demande la difficulté à propos de nbr de retraites
         pass
 
-    def jouerSudoku(self): # prends la difficulté, la grille complete, fait la grille prete à resoudre et appele boucleDeJeu
+    def playSudoku(self): # prends la difficulté, la grille complete, fait la grille prete à resoudre et appele gameLoop
         pass
 
-    def boucleDeJeu(self): # soit entrer une valeur(appelle jouerCoup()), soit donner la grille complete, soit quitter
+    def gameLoop(self): # soit entrer une valeur(appelle playMove()), soit donner la grille complete, soit quitter
         pass
 
-    def jouerCoup(self): # entrer une certain valeur sur un certian ligne et colenne, verifie si cest bon en comparaison avec la grille complete
+    def playMove(self): # entrer une certain valeur sur un certian ligne et colenne, verifie si cest bon en comparaison avec la grille complete
         pass
 
-    def donnerGrilleComplete(self): # donne une grille complete pour traité dans jouerSudoku
+    def giveFullGrid(self): # donne une grille complete pour traité dans playSudoku
         pass
 
-    def quitter(self): # ou tout simplement break
+    def quit(self): # ou tout simplement break
         pass
