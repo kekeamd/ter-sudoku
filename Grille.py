68c6d9ae69c201c0b017a157ad89506daac526f0
Barré-Guétrot_Ilan <ibarreguetrot@gmail.com>
2026-02-08 16:27:27 +0100

    changement de la taille pour matcher la manière dont ça a était fait dans zone

--- Grille.py
+++ Grille.py
@@ -11,15 +11,15 @@ from math import sqrt
 class Grille(ABC):
     def __init__(self):
         self.__grille : list[Zone] = []
-        self.__size : int = 9
-        for _ in range(self.__size):
+        self.__sizeCote : int = 3
+        for _ in range(self.__sizeCote*self.__sizeCote):
             self.__grille.append(Zone())
         self.__difficulte : Difficulte = None
-    def __init__(self, zoneList : list[Zone] , size : int = 9): #sizeCote à revoir (est-ce vraiment utile?? checks supplémentaire à faire???)
-        if (size!=len(zoneList)):
+    def __init__(self, zoneList : list[Zone] , sizeCote : int = 3):
+        if ((sizeCote*sizeCote)!=len(zoneList)):
             raise(GrilleError("Grille : deuxième argument invalide ou alors la taille de 'zoneList' est différente de la valeur par défaut (avez-vous pensé à préciser la taille?)"))
         self.__grille : list[Zone] = zoneList
-        self.__size : int = size
+        self.__sizeCote : int = sizeCote
         self.__difficulte : Difficulte = None
     
 
@@ -42,20 +42,18 @@ class Grille(ABC):
     #purpose: renvoie la ligne numéro 'row'
     def getRow(self, row : int) -> list[int]: #row commence à 0
         rowValues = []
-        l = sqrt(self.__size)//1 # l <-- nombre de zones dans la ligne
-        for i in range(l):
-            zone = self.__grille[indexOfFirstZoneInRow(row, l) + i] #avec i qui sert d'offset par rapport à la première zone de la ligne
-            rowValues+= zone.getRow(indexRowOrColumnInZone(row, l)) #on on concatène la liste de valeurs actuel avec la liste de valeurs dans la ligne de 'zone'
+        for i in range(self.__sizeCote):
+            zone = self.__grille[indexOfFirstZoneInRow(row, self.__sizeCote) + i] #avec i qui sert d'offset par rapport à la première zone de la ligne
+            rowValues+= zone.getRow(indexRowOrColumnInZone(row, self.__sizeCote)) #on on concatène la liste de valeurs actuel avec la liste de valeurs dans la ligne de 'zone'
         return rowValues
 
 
     #purpose: renvoie la colonne numéro 'column'
     def getColumn(self, column : int) -> list[int]:
         colValues = []
-        l = sqrt(self.__size)//1 # l <-- nombre de zones dans la colonne
-        for i in range(l):
-            zone = self.__grille[indexOfFirstZoneInColumn(column, l) + l*i] #avec l*i qui sert d'offset par rapport à la première zone de la colonne
-            colValues+= zone.getColum(indexRowOrColumnInZone(column, l)) #on on concatène la liste de valeurs actuel avec la liste de valeurs dans la colonne de 'zone'
+        for i in range(self.__sizeCote):
+            zone = self.__grille[indexOfFirstZoneInColumn(column, self.__sizeCote) + self.__sizeCote*i] #avec sizeCote*i qui sert d'offset par rapport à la première zone de la colonne
+            colValues+= zone.getColum(indexRowOrColumnInZone(column, self.__sizeCote)) #on on concatène la liste de valeurs actuel avec la liste de valeurs dans la colonne de 'zone'
         return colValues
 
 
@@ -71,8 +69,7 @@ class Grille(ABC):
 
     #purpose : renvoie la cellule aux coordonnées ('row', 'column')
     def __getCelluleCoord(self, row : int, column : int) -> Cellule:
-        l = sqrt(self.__size)//1 # l <-- nombre de lignes/colonnes dans la zone
-        zone = self.__grille[indexOfZone(row, column, l)]
+        zone = self.__grille[indexOfZone(row, column, self.__sizeCote)]
         cellule = zone.getCelluleCoord(row, column)
         return cellule
 
@@ -91,7 +88,7 @@ class Grille(ABC):
 
     #purpose: rectifie les listes de candidats des cellules de la zone 'zone'
     def __adjustCandidatesZone(self, zone : Zone) -> None:
-        for i in range(self.__size):
+        for i in range(self.__sizeCote*self.__sizeCote):
             cellule =  self.__getCelluleIndex(zone, i)
             impossible = [valuesWithoutZero(zone.getValues())]
             self.__adjustCandidatesCellule(cellule, impossible)
@@ -99,7 +96,7 @@ class Grille(ABC):
 
     #purpose: rectifie les listes de candidats des cellules de la ligne 'row'
     def __adjustCandidatesRow(self, row : int) -> None:
-        for i in range(self.__size):
+        for i in range(self.__sizeCote*self.__sizeCote):
             cellule =  self.__getCelluleCoord(row, i)
             impossible = [valuesWithoutZero(self.getRow(row))]
             self.__adjustCandidatesCellule(cellule, impossible)
@@ -108,7 +105,7 @@ class Grille(ABC):
 
     #purpose: rectifie les listes de candidats des cellules de la colonne 'column'
     def __adjustCandidatesColumn(self, column : int) -> None:
-        for i in range(self.__size):
+        for i in range(self.__sizeCote*self.__sizeCote):
             cellule =  self.__getCelluleCoord(i, column)
             impossible = [valuesWithoutZero(self.getColumn(column))]
             self.__adjustCandidatesCellule(cellule, impossible)
@@ -116,7 +113,7 @@ class Grille(ABC):
 
     #purpose: rectifie les listes de candidats des cellules de la grille
     def adjustCandidates(self) -> None:
-        size = self.__size
+        size = self.__sizeCote*self.__sizeCote
         for i in range(size):
             self.__adjustCandidatesZone(self.__grille[i])
             self.__adjustCandidatesRow(i)
@@ -125,8 +122,7 @@ class Grille(ABC):
 
     #purpose: rectifie la liste de candidats de la cellule aux coordonnées ('row', 'column')
     def adjustCandidatesAfterAddingValueCoord(self, row : int, column : int) -> None:
-        l = sqrt(self.__size)//1 # l <-- nombre de lignes/colonnes dans la zone
-        zone = self.__grille[indexOfZone(row, column, l)]
+        zone = self.__grille[indexOfZone(row, column, self.__sizeCote)]
         self.__adjustCandidatesZone(zone)
         self.__adjustCandidatesRow(row)
         self.__adjustCandidatesColumn(column)
@@ -134,15 +130,14 @@ class Grille(ABC):
 
     #purpose: rectifie la liste de candidats de la cellule d'index 'index' dans la zone 'zone'
     def adjustCandidatesAfterAddingValueIndex(self, zone : Zone, index : int) -> None:
-        l = sqrt(self.__size)//1 # l <-- nombre de lignes/colonnes
         zoneI = -1
-        for z in range(self.__size):
+        for z in range(self.__sizeCote*self.__sizeCote):
             if (self.__grille[z]==zone):
                 zoneI = z
         if (zoneI==-1):
             raise(GrilleError("Grille : impossible d'identifier la zone en ajustant les candidats après modification de la valeur d'une cellule par index"))
-        row = indexOfRow(zoneI, index, l)
-        column = indexOfColumn(zoneI, index, l)
+        row = indexOfRow(zoneI, index, self.__sizeCote)
+        column = indexOfColumn(zoneI, index, self.__sizeCote)
         self.__adjustCandidatesZone(zone)
         self.__adjustCandidatesRow(row)
         self.__adjustCandidatesColumn(column)
@@ -188,14 +183,14 @@ class Grille(ABC):
     #purpose: clone la grille (duh!)
     def clone(self):# -> Grille
         newGrille = []
-        for i in range(self.__size):
+        for i in range(self.__sizeCote*self.__sizeCote):
             newGrille.append(self.__grille[i].clone())
         return Grille(newGrille)
 
 
     #purpose: affiche la grille
     def printGrille(self) -> None:
-        size = self.__size
+        size = self.__sizeCote*self.__sizeCote
         for row in range(size):
             line = ""
             for column in range(size):
@@ -211,7 +206,7 @@ class Grille(ABC):
 
     #purpose: renvoie la grille sous forme de chaine de caractères
     def toString(self) -> str:
-        size = self.__size
+        size = self.__sizeCote*self.__sizeCote
         s = "[ "
         for i in range(size):
             s +=self.__grille[i].toString()
