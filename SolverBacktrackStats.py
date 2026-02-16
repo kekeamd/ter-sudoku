b098ff0bed91fe06a1f16381167381575dacca15
Daphné <abdourahamane-abdoul.dolo01@etu.umontpellier.fr>
2026-02-16 17:49:14 +0100

    maj solverbactrackStats

--- SolverBacktrackStats.py
+++ SolverBacktrackStats.py
@@ -1,5 +1,5 @@
 
-import SolverBacktrack
+from SolverBacktrack import SolverBacktrack
 from Grille import Grille
 
 class SolverBacktrackStats(SolverBacktrack): #transformation de la classe en classe static parce qu'on ne veut absolument pas instancier des solvers T-T
